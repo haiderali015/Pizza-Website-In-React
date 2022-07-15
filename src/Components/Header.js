171478c114449da41239d30da8188bf434cf67eb
haider ali <ha347278@gmail.com>
2022-07-16 04:05:45 +0500

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,8 +8,9 @@ const Header = () => {
     <div className='container'>
     <div className='banner_text'>
     <h3>Pizza Delivery</h3>
-    <h1>PizzA HuB</h1>
-    <p>PizzA HuB is home to the most delicious pizza in Lahore, Pakistan. ORDER NOW from our website and get your food craving</p>
+    <h1 style={{fontFamily:'sans-serif'}}>PizzA <span style={{ color: "black" , background: 'rgb(255, 145, 39)', borderRadius:'1vw', fontFamily:'sans-serif', position: 'relative'}}>
+    hub</span></h1>
+    <p>PizzA <span style={{ color: 'rgb(255, 145, 39)'}}>HuB</span> is most delicious pizza shop in Lahore, Pakistan. ORDER NOW from our website and get your food craving</p>
     </div>
     </div>
     </div>    
