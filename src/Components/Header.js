87f4982a21b333499df84daf41d77581cc7a6272
haider ali <ha347278@gmail.com>
2022-07-16 03:32:59 +0500

    text setting

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,9 +6,11 @@ const Header = () => {
     <div className='banner'>
     <div className='banner__content'>
     <div className='container'>
-
+    <div className='banner_text'>
     <h3>Pizza Delivery</h3>
-
+    <h1>PizzA HuB</h1>
+    <p>PizzA HuB is home to the most delicious pizza in Lahore, Pakistan. ORDER NOW from our website and get your food craving</p>
+    </div>
     </div>
     </div>    
     </div>
