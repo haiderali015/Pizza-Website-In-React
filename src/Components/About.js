c611a90e3e9e4fcf0a24e4021c149bf32ef421b3
haider ali <ha347278@gmail.com>
2022-07-16 19:43:41 +0500

    about section adding

---
+++ src/Components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+        <h3>About Us</h3>
+        <h1>Welcome to Pizza HuB</h1>
+        <p>PizzA <span style={{ color: 'rgb(255, 145, 39)'}}>HuB</span> is most delicious pizza shop in Lahore, Pakistan. ORDER NOW from our website and get your food craving</p>
+    </>
+  )
+}
+
+export default About
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,9 +13,9 @@ const Header = () => {
     <h3>Pizza Delivery</h3>
     <h1 style={{fontFamily:'sans-serif'}}>PizzA <span style={{ color: "black" , background: 'rgb(255, 145, 39)', borderRadius:'1vw', fontFamily:'sans-serif'}}>hub</span></h1>
     <p>PizzA <span style={{ color: 'rgb(255, 145, 39)'}}>HuB</span> is most delicious pizza shop in Lahore, Pakistan. ORDER NOW from our website and get your food craving</p>
-        <div className='banner-btn'>
-        <a href='btn btn-smart'>Delivery Now</a>
-        </div>
+    <div className='banner-btn'>
+    <a href="" className='btn btn-smart'>Delivery Now</a>
+    </div>
     </div>
           </div>
       </div>    
