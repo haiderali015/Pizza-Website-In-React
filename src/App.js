dfd649bc0fbbcc0d7e76fa56bd45d0bf5ac69f44
haider ali <ha347278@gmail.com>
2022-07-17 03:12:30 +0500

    shop now section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Header';
 import About from './Components/About';
+import Shop from './Components/Shop';
 
 function App() {
   return (
     <>
         <Header/>
         <About/>
+        <Shop/>
     </>
   );
 }
---
+++ src/Components/Shop.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <>
+        <div className='shop'>
+            <div className='container'>
+                <div className='shop_text'>
+                    <h1>Free Delivery with Pizza of the Day</h1>
+                    <div className='shop_btn'>
+                        <a href='' className='btn btn-smart'>Shop Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Shop
