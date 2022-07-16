1d94f5e66316dfcff0fbd6f77819ee404de8755d
haider ali <ha347278@gmail.com>
2022-07-16 17:53:01 +0500

    logos added

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar_text'>
+                <div className='logo'>
+                    <img src='public\images\logbg.jpg'/>
+                </div>
+            </div>
+        
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
