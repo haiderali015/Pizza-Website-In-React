9d705dc439426c63d9559b97875a57c814d06972
haider ali <ha347278@gmail.com>
2022-07-16 20:28:12 +0500

    about sec

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,11 +5,22 @@ const About = () => {
     <>
     <div className='about'>
     <div className='container'>
-        <h3>About Us</h3>
-        <h1>Welcome to Pizza HuB</h1>
-        <p>PizzA <span style={{ color: 'rgb(255, 145, 39)'}}>HuB</span> is most delicious pizza shop in Lahore, Pakistan. ORDER NOW from our website and get your food craving</p>
-        <div className='banner-btn'>
-        <a href="" className='btn btn-smart'>Delivery Now</a>
+        <div className='row'>
+        <div className='col-6 p-25'>
+            <h3>About Us</h3>
+            <h1>Welcome to Pizza HuB</h1>
+            <p>PizzA <span style={{ color: 'rgb(255, 145, 39)'}}>HuB</span> is most delicious pizza shop in Lahore, Pakistan. ORDER NOW from our website and get your food craving</p>
+            <div className='banner-btn'>
+            <a href="" className='btn btn-smart'>Delivery Now</a>
+            </div>
+        </div>
+
+            <div className='col-6'>
+            <div className='about_img'>
+                <img src='/images/aboutpic.jpg' alt='pizza'/>
+            </div>
+
+            </div>
         </div>
     </div>
     </div>
